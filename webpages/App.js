5be890485218d888d51716a7a9e016ce2090b51e
Woondu <73076434+MFDOOM-ab@users.noreply.github.com>
2023-11-09 18:03:21 -0500

    Homepage files and App.js
    Graft to connection files in main folders

--- webpages/App.js
+++ webpages/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import LoginPage from './LoginPage';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LoginPage from './LoginPage.js';
+import HomePage from './HomePage.js'; // Imported HomePage.html thru homepage.js
 
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route path="/login" component={LoginPage} />
-        <Route path="/homepage">
-        </Route>
-      </Switch>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/homepage" element={<HomePage />} />
+        <Route path="/" element={<LoginPage />} />
+      </Routes>
     </Router>
   );
 }
---
+++ webpages/HomePage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <iframe 
+      src="/homepage.html" 
+      title="Home Page" 
+      width="100%" 
+      height="100%" 
+      style={{ border: 'none' }}
+    />
+  );
+};
+
+export default HomePage;
